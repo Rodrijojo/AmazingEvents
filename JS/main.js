951662208cbdecfe66d2c77d459110491ba46b4d
Rodrijojo <marcosrodriguez3000@gmail.com>
2023-09-07 18:56:01 -0300

    ordené las funciones y cree dinamicamente los checkboxes

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,57 @@
 const eventos = data.events;
 
+// Cards
+
 const cardsContainer = document.getElementById("cardsContainer")
 
-let card = "";
+function crearCard(eventos) {
+  let card = "";
 
-for (const evento of eventos) {
-    card += `<div class="card" style="width: 18rem;">
-    <div class="card-body d-flex flex-column justify-content-center align-items-center">
-      <img src="${evento.image}" alt="Food fair" class="card-img">
-      <h5 class="card-title">${evento.name}</h5>
-      <p class="card-text">${evento.description}</p>
-      <div class="d-flex justify-content-around align-items-baseline gap-4">
-        <p>$${evento.price}</p>
-        <button type="button" class="btn btn-primary"><a href="Details.html#Details1" class="card-link color-details">Details</a></button>                  
+  for (const evento of eventos) {
+      card += `<div class="card" style="width: 18rem;">
+      <div class="card-body d-flex flex-column justify-content-center align-items-center">
+        <img src="${evento.image}" alt="Food fair" class="card-img">
+        <h5 class="card-title">${evento.name}</h5>
+        <p class="card-text">${evento.description}</p>
+        <div class="d-flex justify-content-around align-items-baseline gap-4">
+          <p>$${evento.price}</p>
+          <button type="button" class="btn btn-primary"><a href="Details.html#Details1" class="card-link color-details">Details</a></button>                  
+        </div>
       </div>
-    </div>
-  </div>`
+    </div>`
+  }
+
+  cardsContainer.innerHTML = card;
 }
 
-cardsContainer.innerHTML = card;
+crearCard(eventos);
+
+// Checkbox
+
+let checkboxContainer = document.querySelector("#checkbox");
+
+function crearCheckbox(eventos) {
+  const categorias = [...new Set(eventos.map(evento => evento.category))]
+  console.log(categorias)
+  let checkbox = ""
+
+  for (const categoria of categorias) {
+    checkbox += `<div class="form-check">
+                  <input class="form-check-input category_filter" type="checkbox" value="" id="flexCheckDefault">
+                  <label class="form-check-label" for="flexCheckDefault">
+                    <p>${categoria}</p>
+                  </label>
+                 </div>`
+  }
+
+  checkboxContainer.innerHTML = checkbox;
+}
+
+crearCheckbox(eventos)
+
+/*
+document.addEventListener("DOMContentLoaded", function () {
+  const checkboxes = document.querySelectorAll(".categoty_filter");
+
+});
+*/
--- JS/upcoming.js
+++ JS/upcoming.js
@@ -1,18 +1,13 @@
 const eventos = data.events;
 
+// Cards
+
 var currentDate = data.currentDate;
-
 currentDate = currentDate.split("-");
-
-console.log(currentDate);
 
 const cardsContainer = document.getElementById("cardsContainer")
 
-let card = "";
 
-
-//compararFecha()
-// separar la fecha que viene como string en array y comparar elemento a elemento con if
 
 function compararFecha(fecha) {
     let fechaSplit = fecha.split("-");
@@ -26,34 +21,25 @@ function compararFecha(fecha) {
     return "igual";
 }
 
-function restringeFecha(evento){
-    let date = evento.date
+function crearCard(eventos){
+    let card = "";
+    for (const evento of eventos) {
+        if(compararFecha(evento.date) === "mayor"){
+            card += `<div class="card" style="width: 18rem;">
+                        <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                            <img src="${evento.image}" alt="Food fair" class="card-img">
+                            <h5 class="card-title">${evento.name}</h5>
+                            <p class="card-text">${evento.description}</p>
+                            <div class="d-flex justify-content-around align-items-baseline gap-4">
+                                <p>$${evento.price}</p>
+                                <button type="button" class="btn btn-primary"><a href="Details.html#Details1" class="card-link color-details">Details</a></button>                  
+                            </div>
+                        </div>
+                    </div>`
+        }
+    }
 
-    if(compararFecha(date) === "mayor"){
-        return true;
-    }
-    else{
-        return false;
-    }
+    cardsContainer.innerHTML = card;
 }
 
-
-for (const evento of eventos) {
-    if(restringeFecha(evento) === true){
-                    card += `<div class="card" style="width: 18rem;">
-        <div class="card-body d-flex flex-column justify-content-center align-items-center">
-            <img src="${evento.image}" alt="Food fair" class="card-img">
-            <h5 class="card-title">${evento.name}</h5>
-            <p class="card-text">${evento.description}</p>
-            <div class="d-flex justify-content-around align-items-baseline gap-4">
-                <p>$${evento.price}</p>
-                <button type="button" class="btn btn-primary"><a href="Details.html#Details1" class="card-link color-details">Details</a></button>                  
-            </div>
-            </div>
-        </div>`
-    }
-}
-
-
-
-cardsContainer.innerHTML = card;
+crearCard(eventos)
